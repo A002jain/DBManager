a3d788a34b5c07189550561d13102db92b72db5e
Abhinav Jain <48998860+A002jain@users.noreply.github.com>
2021-12-23 18:20:52 +0530

    Update MainVerticle.java

--- src/main/java/com/example/starter/MainVerticle.java
+++ src/main/java/com/example/starter/MainVerticle.java
@@ -12,7 +12,6 @@ import java.util.HashMap;
 
 public class MainVerticle extends AbstractVerticle {
 
-  error
   private static final Logger logger = LoggerFactory.getLogger(MainVerticle.class);
 
   @Override
