3691657b544634a742703eab26df51041369252a
Abhinav Jain <ajchirawa@gmail.com>
2021-12-23 19:22:33 +0530

    set host and port

--- src/main/java/com/example/starter/MainVerticle.java
+++ src/main/java/com/example/starter/MainVerticle.java
@@ -31,7 +31,7 @@ public class MainVerticle extends AbstractVerticle {
      
       .requestHandler(router)
       
-      .listen(http -> {
+      .listen("0.0.0.0",8888,http -> {
       if (http.succeeded()) {
         startPromise.complete();
         logger.info("HTTP server started on port 8888");
